16e752ef7353cbce61f380e0f7cf02cdfa2b0634
r1oga <r1oga@tuta.io>
2020-03-29 22:20:29 +0200

    Updates

---
+++ precache-manifest.9ff92d00d1b0bba2bfb035dd6def4145.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a95cfd0c81329d9ed40103023de5a296",
+    "url": "/r1oga/dj-covid/index.html"
+  },
+  {
+    "revision": "c0e1c3d20781d6ab6ac8",
+    "url": "/r1oga/dj-covid/static/js/2.1657ced5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/r1oga/dj-covid/static/js/2.1657ced5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d93e0e0b5c3eadc41c0",
+    "url": "/r1oga/dj-covid/static/js/main.7748ea85.chunk.js"
+  },
+  {
+    "revision": "7e8397b37675886369dc",
+    "url": "/r1oga/dj-covid/static/js/runtime-main.b3938b15.js"
+  }
+]);
